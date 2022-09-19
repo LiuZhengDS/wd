c9c9ccb73f9cb39853f5b8187568a5ed1c167391
LiuZhengDS <liuzh0110@gmail.com>
2022-09-19 11:19:10 +0800

    Sep19

--- visual/urls.py
+++ visual/urls.py
@@ -3,12 +3,13 @@ from . import views
 
 app_name = 'visual'  # 这句是必须的，和之后所有的URL语句有关
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path(r'index', views.index, name='index'),
     # path(r'^add_query', views.add_query)
     path(r'query', views.query, name="query"),
-    path('blog', views.blog, name = 'blog'),
+    path(r'blog', views.blog, name = 'blog'),
     path(r'showdata', views.showdata, name="showdata"),
     path(r'plot', views.plot, name="plot"),
+    path(r'choose_file', views.choose_file, name="choose_file"),
 ]
 
 
