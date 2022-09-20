2f7268dc9fc133e099c8ccfd7a6ac40b50988299
LiuZhengDS <liuzh0110@gmail.com>
2022-09-20 09:11:42 +0800

    Sep20

--- visual/views.py
+++ visual/views.py
@@ -20,21 +20,6 @@ target_path = 'C:/Users/1000300246/Desktop/'
 
 global pth
 global DF
-<<<<<<< HEAD
-global pth
-pth = root_path
-# DF = pd.read_csv(root_path + file_name)
-
-def choose_file_name(request):
-
-    mselect_dict = {}
-    file_name = request.POST.get("file_name")
-    print("==========", file_name, "==============")
-    global DF
-    DF = pd.read_csv(pth + file_name)
-    print('Load file: %s' % file_name)
-    print('Global data:\n',  DF)
-=======
 def choose_file_name(request):
     global DF
     mselect_dict = {}
@@ -42,7 +27,6 @@ def choose_file_name(request):
     print("==========", file_name, "==============")
 
     DF = pd.read_csv(pth + file_name)
->>>>>>> da666ad85258287b72907c59fc5b26cfb082d744
     df = DF.iloc[0:50]
     dct = columns2dictionary(df)
     for key, value in dct.items():
@@ -51,13 +35,7 @@ def choose_file_name(request):
         mselect_dict[key]['options'] = get_distinct_list(df, value)  # 以后可以后端通过列表为每个多选控件传递备选项
     context = {
         'mselect_dict': mselect_dict,
-<<<<<<< HEAD
-        'DF': df.to_html(),
     }
-
-=======
-    }
->>>>>>> da666ad85258287b72907c59fc5b26cfb082d744
     return render(request, 'visual/display.html', context)
 
 def test_time():
@@ -159,34 +137,21 @@ def search(request, column, kw, df):
         }
     return HttpResponse(json.dumps(res, ensure_ascii=False), content_type="application/json charset=utf-8") # 返回结果必须是json格式
 
-<<<<<<< HEAD
-=======
 
 
 
 
->>>>>>> da666ad85258287b72907c59fc5b26cfb082d744
 def showdata(request):
     data = dict(six.iterlists(request.GET))
     print('data is:', data)
     con = {
-<<<<<<< HEAD
-        'alldata': DF.to_html(),
-=======
         'alldata': DF.iloc[:30, :].to_html(),
->>>>>>> da666ad85258287b72907c59fc5b26cfb082d744
     }
     return HttpResponse(json.dumps(con, ensure_ascii=False),
                         content_type="application/json charset=utf-8")
 
 def query(request):
-<<<<<<< HEAD
-
     form_dict = dict(six.iterlists(request.GET))
-    print(form_dict, 11111111111111111)
-=======
-    form_dict = dict(six.iterlists(request.GET))
->>>>>>> da666ad85258287b72907c59fc5b26cfb082d744
     df = DF.iloc[0:30]
     box = []
 
@@ -233,12 +198,9 @@ def plot(request):
     return HttpResponse(json.dumps(context, ensure_ascii=False),
                         content_type="application/json charset=utf-8")
 
-<<<<<<< HEAD
-=======
 
 pth = 'C:/Users/1000300246/Desktop/'
 
->>>>>>> da666ad85258287b72907c59fc5b26cfb082d744
 def index(request, pth=pth):
     # form_dict = dict(six.iterlists(request.GET))
     file_list = []
@@ -274,19 +236,3 @@ def blog(request):
         'mselect_dict': mselect_dict,
     }
     return render(request, 'visual/blog_main_display.html', context)
-<<<<<<< HEAD
-
-
-# def choose_file(request):
-#     file_list_dict = dict(six.iterlists(request.GET))
-#     selected_file = file_list_dict['file_selection'][0]
-#     print(selected_file)
-#     selected_file_dct = {
-#
-#         'file_selection': selected_file,
-#     }
-#     return HttpResponse(json.dumps(selected_file_dct, ensure_ascii=False),
-#
-#                         content_type="application/json charset=utf-8")
-=======
->>>>>>> da666ad85258287b72907c59fc5b26cfb082d744
